f496502168e3ff8618cd62953f85ffa6e3474e83
atilio nogueira <atilion@yahoo.com.br>
2025-04-17 15:07:21 -0300

    Configurando o Tema da API

---
+++ Ucode.Web/wwwroot/Configuration.cs
@@ -0,0 +1,45 @@
+﻿using MudBlazor.Utilities;
+using MudBlazor;
+
+namespace Ucode.Web.wwwroot
+{
+    public static class Configuration
+    {
+        public static MudTheme Theme = new()
+        {
+            Typography = new Typography
+            {
+                Default = new Default
+                {
+                    FontFamily = ["Raleway", "sans-serif"]
+                }
+            },
+            Palette = new PaletteLight
+            {
+                Primary = new MudColor("#0D47A1"), // Azul escuro
+                PrimaryContrastText = Colors.Shades.White, // Bom contraste com azul escuro
+                Secondary = Colors.BlueGrey.Default, // Azul acinzentado para suavizar
+                Background = Colors.Grey.Lighten5, // Fundo bem claro
+                AppbarBackground = new MudColor("#0D47A1"), // Mesma cor da primária
+                AppbarText = Colors.Shades.White,
+                TextPrimary = Colors.Grey.Darken3, // Texto escuro, mas não preto
+                DrawerText = Colors.Shades.White,
+                DrawerBackground = Colors.BlueGrey.Darken4 // Azul acinzentado escuro no menu
+            },
+            PaletteDark = new PaletteDark
+            {
+                Primary = Colors.Blue.Lighten2, // Azul claro para se destacar no escuro
+                PrimaryContrastText = Colors.Shades.White, // Texto branco sobre azul claro
+                Secondary = Colors.BlueGrey.Default, // Azul acinzentado como secundária
+                Background = Colors.Grey.Darken4, // Fundo escuro padrão
+                Surface = Colors.Grey.Darken3, // Cartões, modais, etc
+                DrawerBackground = Colors.BlueGrey.Darken4, // Lateral com tom frio
+                DrawerText = Colors.Shades.White,
+                AppbarBackground = Colors.Blue.Darken3, // AppBar azul escuro
+                AppbarText = Colors.Shades.White,
+                TextPrimary = Colors.Shades.White,
+                TextSecondary = Colors.Grey.Lighten1
+            }   
+        };
+    }
+}
